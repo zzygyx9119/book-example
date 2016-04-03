0fc4874874110c5ce61aa098c883f9100bb5eaa8
Harry <hjwp2@cantab.net>
2016-04-03 23:08:04 +0100

    SPIKE HACKED - simplify

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -1,28 +1,30 @@
-# encoding: utf8
-from django.db import models, migrations
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 import django.utils.timezone
+import uuid
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '__first__'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='ListUser',
             fields=[
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
-                ('is_superuser', models.BooleanField(help_text='Designates that this user has all permissions without explicitly assigning them.', default=False, verbose_name='superuser status')),
-                ('email', models.EmailField(max_length=75, serialize=False, primary_key=True)),
-                ('groups', models.ManyToManyField(blank=True, to='auth.Group', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, to='auth.Permission', verbose_name='user permissions')),
+                ('email', models.EmailField(serialize=False, max_length=254, primary_key=True)),
+                ('last_login', models.DateTimeField(default=django.utils.timezone.now)),
             ],
-            options={
-                'abstract': False,
-            },
-            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Token',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('email', models.EmailField(max_length=254)),
+                ('uid', models.CharField(max_length=36, default=uuid.uuid4)),
+            ],
         ),
     ]
--- accounts/models.py
+++ accounts/models.py
@@ -1,34 +1,19 @@
 import os
 import uuid
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
 class Token(models.Model):
     email = models.EmailField()
     uid = models.CharField(max_length=36, default=uuid.uuid4)
 
 
-class ListUserManager(BaseUserManager):
-
-    def create_user(self, email):
-        ListUser.objects.create(email=email)
-
-    def create_superuser(self, email, password):
-        self.create_user(email)
-
-
-class ListUser(AbstractBaseUser, PermissionsMixin):
+class ListUser(models.Model):
     email = models.EmailField(primary_key=True)
+    last_login = models.DateTimeField(default=timezone.now)
     USERNAME_FIELD = 'email'
-    #REQUIRED_FIELDS = ['email', 'height']
+    REQUIRED_FIELDS = ()
 
-    objects = ListUserManager()
-
-    @property
-    def is_staff(self):
-        return self.email == 'harry.percival@gmail.com'
-
-    @property
-    def is_active(self):
-        return True
+    # def is_authenticated(self):
+        # return True
 
