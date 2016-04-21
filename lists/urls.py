f3844681e872990eaf8a1a8231d46497a8857b46
Harry <hjwp2@cantab.net>
2016-04-21 18:03:39 +0100

    despike lists

--- lists/urls.py
+++ lists/urls.py
@@ -5,8 +5,6 @@ from lists import views
 urlpatterns = [
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
-    url(r'^(\d+)/items/$', views.list_items, name='list_items'),
-    url(r'^(\d+)/items/add$', views.add_to_list, name='add_to_list'),
     url(r'^users/(.+)/$', views.my_lists, name='my_lists'),
 ]
 
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,9 @@
-import json
 from django.shortcuts import redirect, render
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
+User = get_user_model()
 
 from lists.forms import ExistingListItemForm, ItemForm, NewListForm
 from lists.models import List
-
-User = get_user_model()
 
 
 def home_page(request):
@@ -24,22 +21,12 @@ def new_list(request):
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     form = ExistingListItemForm(for_list=list_)
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
-
-
-def list_items(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    return HttpResponse(json.dumps(
-        [{'id': item.id, 'text': item.text} for item in list_.item_set.all()]
-    ), content_type='application/json')
-
-
-def add_to_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    form = ExistingListItemForm(for_list=list_, data=request.POST)
-    if form.is_valid():
-        form.save()
-        return HttpResponse(json.dumps(dict(status="OK")), content_type="application/json")
 
 
 def my_lists(request, email):
