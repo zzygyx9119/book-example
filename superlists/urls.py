ddd0e11fd671a6b80cba2edabf5c783b5c08c0ce
Harry <hjwp2@cantab.net>
2016-04-21 18:03:51 +0100

    start despiking with api tests

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
+from lists import api_urls
 from accounts import urls as account_urls
 
 urlpatterns = [
@@ -8,5 +9,6 @@ urlpatterns = [
     url(r'^lists/', include(list_urls)),
     url(r'^accounts/', include(account_urls)),
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(api_urls)),
 ]
 
