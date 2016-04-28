7fc8b75fcc131a7651c67d1ffd5452e9fecca006
Harry <hjwp2@cantab.net>
2016-04-28 10:12:53 +0100

    first cut of a js test for on-load ajax get

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,14 +1,28 @@
 /*global $ */
 
-var initialize = function() {
+var initialize = function(options) {
     $('input').on('keypress', function () {
         $('.has-error').hide();
     });
+
+    if (options) {
+        $.get(options.getItemsUrl).then(function (response) {
+            var rows = '';
+            for (var i=0; i<response.length; i++) {
+                var item = response[i];
+                console.log(item);
+                rows += '\n<tr><td>' + (i+1) + ': ' + item.text + '</td></tr>';
+            }
+            $('#id_list_table').html(rows);
+            console.log($('#id_list_table').html());
+        });
+    }
+
 };
 
 
 window.Superlists = window.Superlists || {};
 window.Superlists.Lists = window.Superlists.Lists || {
-    initialize: initialize
+    initialize: initialize,
 };
 
