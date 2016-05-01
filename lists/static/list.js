6a82b9feae19ac66c199903c05f04f5a4d1a7e68
Harry <hjwp2@cantab.net>
2016-05-01 12:55:56 +0100

    slightly different setup, allows tests with mocks

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,46 +1,45 @@
 /*global $ */
 
-var initialize = function(options) {
-    $('input').on('keypress', function () {
-        $('.has-error').hide();
-    });
+window.Superlists = window.Superlists || {};
+window.Superlists.Lists = {
 
-    if (options && options.listItemsUrl) {
-        var getItems = function () {
-            $.get(options.listItemsUrl).then(function (response) {
-                var rows = '';
-                for (var i=0; i<response.length; i++) {
-                    var item = response[i];
-                    rows += '\n<tr><td>' + (i+1) + ': ' + item.text + '</td></tr>';
-                }
-                $('#id_list_table').html(rows);
-            });
-        };
-
-        var form = $('#id_item_form');
-
-        form.on('submit', function(event) {
-            event.preventDefault();
-            $.post(options.listItemsUrl, {
-                'text': form.find('input[name="text"]').val(),
-                'csrfmiddlewaretoken': form.find('input[name="csrfmiddlewaretoken"]').val(),
-            }).success(function () {
-                getItems();
-                $('.has-error').hide();
-            }).error(function (xhr) {
-                $('.has-error').show();
-                $('.has-error .help-block').text(JSON.parse(xhr.responseText).error);
-            });
+    initialize: function (options) {
+        $('input').on('keypress', function () {
+            $('.has-error').hide();
         });
 
-        getItems();
+        if (options && options.listItemsUrl) {
+            url = options.listItemsUrl;
+            $('#id_item_form').on('submit', function(event) {
+                event.preventDefault();
+                window.Superlists.Lists.postItem(options.listItemsUrl);
+            });
+            window.Superlists.Lists.getItems(options.listItemsUrl);
+        }
+    },
+
+    getItems: function (url) {
+        $.get(url).success(function (response) {
+            var rows = '';
+            for (var i=0; i<response.length; i++) {
+                var item = response[i];
+                rows += '\n<tr><td>' + (i+1) + ': ' + item.text + '</td></tr>';
+            }
+            $('#id_list_table').html(rows);
+        });
+    },
+
+    postItem: function (url) {
+        var form = $('#id_item_form');
+        $.post(url, {
+            'text': form.find('input[name="text"]').val(),
+            'csrfmiddlewaretoken': form.find('input[name="csrfmiddlewaretoken"]').val(),
+        }).success(function () {
+            window.Superlists.Lists.getItems(url);
+            $('.has-error').hide();
+        }).error(function (xhr) {
+            $('.has-error').show();
+            $('.has-error .help-block').text(JSON.parse(xhr.responseText).error);
+        });
     }
-
 };
-
-
-window.Superlists = window.Superlists || {};
-window.Superlists.Lists = window.Superlists.Lists || {
-    initialize: initialize,
-};
-
