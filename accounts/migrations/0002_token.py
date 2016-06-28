0280a1fbd624520a94b1c24c4fd9b203f1e5cd47
Harry <hjwp2@cantab.net>
2016-06-28 23:30:08 +0100

    fix migration for finalised token model. --ch15l036--

--- accounts/migrations/0002_token.py
+++ accounts/migrations/0002_token.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -14,9 +15,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Token',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
                 ('email', models.EmailField(max_length=254)),
-                ('uid', models.CharField(max_length=40)),
+                ('uid', models.CharField(max_length=40, default=uuid.uuid4)),
             ],
         ),
     ]
