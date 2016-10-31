65e8a48235949e0514ca54cbec6868214f8777c6
Harry <hjwp2@cantab.net>
2016-10-31 22:53:06 +0000

    first steps towards using DRF

--- lists/api.py
+++ lists/api.py
@@ -1,31 +1,30 @@
-import json
-from django.http import HttpResponse
-from lists.models import List
-from lists.forms import (
-    ExistingListItemForm,
-    EMPTY_ITEM_ERROR,
-)
+from lists.models import List, Item
+
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class ItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Item
+        fields = ('id', 'text')
 
 
-def list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    if request.method == 'POST':
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse(status=201)
-        else:
-            return HttpResponse(
-                json.dumps({'error': form.errors['text'][0]}),
-                content_type='application/json',
-                status=400
-            )
-    item_dicts = [
-        {'id': item.id, 'text': item.text}
-        for item in list_.item_set.all()
-    ]
-    return HttpResponse(
-        json.dumps(item_dicts),
-        content_type='application/json'
-    )
+class ListSerializer(serializers.ModelSerializer):
+    items = ItemSerializer(many=True, source='item_set')
+
+    class Meta:
+        model = List
+        fields = ('id', 'items',)
+
+
+# ViewSets define the view behavior.
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'lists', ListViewSet)
 
