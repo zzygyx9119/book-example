9a6638e682bfdb02e3d10c4ff2d058dd52ac882d
Harry <hjwp2@cantab.net>
2018-01-11 20:26:06 +0000

    fab function for create session (using gunicorn env vars). --ch18l019--

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -11,3 +11,23 @@ def reset_database(host):
     with settings(host_string=f'elspeth@{host}'):
         run(f'{manage_dot_py} flush --noinput')
 
+
+def _get_server_env_vars(host):
+    pid = run(f'pgrep -n -f {host}/virtualenv/bin/gunicorn').strip()
+    env_lines = run(f'cat /proc/{pid}/environ').split('\x00')
+    current_env = dict(l.split('=') for l in env_lines if l)
+    return {
+        'DJANGO_DEBUG_FALSE': current_env['DJANGO_DEBUG_FALSE'],
+        'DJANGO_SECRET_KEY': current_env['DJANGO_SECRET_KEY'],
+        'SITENAME': current_env['SITENAME'],
+    }
+
+
+def create_session_on_server(host, email):
+    manage_dot_py = _get_manage_dot_py(host)
+    with settings(host_string=f'elspeth@{host}'):
+        env_vars = _get_server_env_vars(host)
+        with shell_env(**env_vars):
+            session_key = run(f'{manage_dot_py} create_session {email}')
+            return session_key.strip()
+
